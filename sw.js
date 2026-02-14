b18354120183adac74793ded37d482b38ebacdab
kojakrejek <kojakrejek@gmail.com>
2026-02-15 00:34:57 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // TUKAR NOMBOR NI SETIAP KALI ADA UPDATE BESAR DI HTML
-const CACHE_NAME = 'fpmsb-v3'; // <--- Tukar jadi v3, v4 bila ada update lagi
+const CACHE_NAME = 'fpmsb-v4'; // <--- Tukar jadi v3, v4 bila ada update lagi
 
 const ASSETS = [
   './',
@@ -40,4 +40,5 @@ self.addEventListener('fetch', (e) => {
     fetch(e.request).catch(() => caches.match(e.request))
   );
 });
+
 
